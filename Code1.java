4a7b6e06f4e48824b589c1bd38e7f5460c4f5977
Pranita-dumbre <pranitadumbre1885@gmail.com>
2026-06-15 18:18:27 +0530

    Create Code1.java

---
+++ Code1.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////
+//
+//  File Name :         Code1.java
+//  Description :       Prints "Jay Ganesh..." to the console.
+//  Author :            Pranita Purushottam Dumbre
+//  Date :              15/06/2026
+//
+/////////////////////////////////////////////////////////////////////
+class Code1
+{
+    ////////////////////////////////////////////////////////////////////
+    //
+    //  Method Name :     main
+    //  Description :     Entry point of the program; prints message.
+    //  Author :          Pranita Purushottam Dumbre
+    //  Date :            15/06/2026
+    //
+    ////////////////////////////////////////////////////////////////////
+    public static void main(String A[])
+    {
+        System.out.println("Jay Ganesh...");
+    }
+}
+
+// Compile :    javac program51.java
+// Run :        java program51
